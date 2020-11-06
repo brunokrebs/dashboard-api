f8bd0abc0790a55a83606d1fe6b9c4a28225df60
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-11-06 09:49:07 -0300

    criando a rota de paginacao

--- src/purchase-order/purchase-order.controller.ts
+++ src/purchase-order/purchase-order.controller.ts
@@ -1,12 +1,42 @@
 import {
   ClassSerializerInterceptor,
   Controller,
+  Get,
+  Query,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { parseBoolean } from '../util/parsers';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PurchaseOrder } from './purchase-order.entity';
+import { PurchaseOrderService } from './purchase-order.service';
 
-@Controller('purchase-order')
+@Controller('purchase-orders')
 @UseGuards(JwtAuthGuard)
 @UseInterceptors(ClassSerializerInterceptor)
-export class PurchaseOrderController {}
+export class PurchaseOrderController {
+  constructor(private purchaseOrderService: PurchaseOrderService) {}
+
+  @Get()
+  async findAll(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('sortedBy') sortedBy: string,
+    @Query('sortDirectionAscending') sortDirectionAscending: string,
+    @Query('query') query: string,
+  ): Promise<Pagination<PurchaseOrder>> {
+    return await this.purchaseOrderService.paginate({
+      page,
+      limit,
+      sortedBy,
+      sortDirectionAscending: parseBoolean(sortDirectionAscending),
+      queryParams: [
+        {
+          key: 'query',
+          value: query,
+        },
+      ],
+    });
+  }
+}
