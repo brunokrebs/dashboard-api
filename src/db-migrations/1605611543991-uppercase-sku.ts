6e535c55f2ccac7449618ee7667f31c10e24fecc
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-11-17 12:57:36 -0300

    correção migration

--- src/db-migrations/1605611543991-uppercase-sku.ts
+++ src/db-migrations/1605611543991-uppercase-sku.ts
@@ -2,10 +2,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class uppercaseSku1605611543991 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('UPDATE product set sku = UPPER(sku)');
-    await queryRunner.query('UPDATE product set sku = Trim(sku)');
-    await queryRunner.query('UPDATE product_variation set sku = UPPER(sku)');
-    await queryRunner.query('UPDATE product_variation set sku = Trim(sku)');
+    await queryRunner.query('UPDATE product SET sku = UPPER(TRIM(sku))');
+    await queryRunner.query(
+      'UPDATE product_variation SET sku = UPPER(TRIM(sku))',
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
