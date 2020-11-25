ffb175befaf644fb908ae7ff488fe393daa1fdee
Bruno Krebs <bruno.krebs@auth0.com>
2020-11-24 21:19:06 -0300

    migration

--- src/db-migrations/1606221222747-calcular-total-purchase-order.ts
+++ src/db-migrations/1606221222747-calcular-total-purchase-order.ts
@@ -3,20 +3,19 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class calcularTotalPurchaseOrder1606221222747
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `UPDATE purchase_order SET total = (subquery.total-purchase_order.discount) FROM
-            (SELECT poi.purchase_order_id,sum(TRUNC(
-                    price*amount,
-                    2))as total   FROM purchase_order_item as poi 
-                             GROUP BY poi.purchase_order_id 
-                             ORDER BY poi.purchase_order_id) as subquery 
-                    WHERE purchase_order.id=subquery.purchase_order_id;`,
-    );
-    await queryRunner.query("UPDATE purchase_order SET status = 'COMPLETED'");
+    await queryRunner.query(`
+      UPDATE purchase_order
+        SET status = 'COMPLETED', total = (subquery.total - purchase_order.discount) FROM (
+          SELECT poi.purchase_order_id, sum(price * amount) AS total 
+            FROM purchase_order_item as poi 
+            GROUP BY poi.purchase_order_id
+        ) AS subquery WHERE purchase_order.id = subquery.purchase_order_id;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('UPDATE purchase_order SET status = null');
-    await queryRunner.query('UPDATE purchase_order SET total =null');
+    await queryRunner.query(
+      'UPDATE purchase_order SET status = null, total = null',
+    );
   }
 }
