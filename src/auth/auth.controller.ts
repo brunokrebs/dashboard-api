c26b3edc5f39513bc820c08dcce18b10b37461d8
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-11-27 17:01:44 -0300

    return token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
@@ -6,6 +13,12 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('refresh-token')
+  async refreshToken(@Body() body) {
+    return this.authService.refreshToken(body.token);
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('sign-in')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,4 +25,8 @@ export class AuthService {
       access_token: this.jwtService.sign(payload),
     };
   }
+
+  async refreshToken(token: string) {
+    return token;
+  }
 }
