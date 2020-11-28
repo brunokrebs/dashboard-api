f31fdb2d84b2dcfaadf3ac1807842ad09f069957
Bruno Krebs <bruno.krebs@auth0.com>
2020-11-28 10:26:34 -0300

    removing useless imports;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  Request,
-  Post,
-  UseGuards,
-  Get,
-  Body,
-} from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
