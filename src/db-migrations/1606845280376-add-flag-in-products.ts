5fd93b388bec0340e51ffcd66fa05f1398b7c850
Bruno Krebs <bruno.krebs@auth0.com>
2020-12-06 19:11:14 -0300

    refactoring migration;

--- src/db-migrations/1606845280376-add-flag-in-products.ts
+++ src/db-migrations/1606845280376-add-flag-in-products.ts
@@ -5,17 +5,16 @@ export class addFlagInProducts1606845280376 implements MigrationInterface {
     await queryRunner.query(
       'ALTER TABLE product ADD COLUMN is_composition boolean;',
     );
+
     await queryRunner.query(`
       UPDATE product SET is_composition = true
-        FROM(SELECT product.id FROM product
-             WHERE product.id IN (SELECT product_id from product_composition))as subquery
-        WHERE  product.id = subquery.id
+      WHERE id IN (
+        SELECT product_id from product_composition
+      );
     `);
+
     await queryRunner.query(`
-      UPDATE product SET is_composition = false
-        FROM(SELECT product.id FROM product
-             WHERE product.id NOT IN (SELECT product_id from product_composition))as subquery
-        WHERE  product.id = subquery.id
+      UPDATE product SET is_composition = false WHERE is_composition IS NULL;
     `);
   }
 
