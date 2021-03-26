89120fabb12949db91aada8d04a6286fc838fe55
Bruno Krebs <bruno.krebs@auth0.com>
2021-03-26 08:56:59 -0300

    backup script;

--- bin/backup/backup-postgres.sh
+++ bin/backup/backup-postgres.sh
@@ -4,17 +4,42 @@ DATE=`date +%Y%m%d`
 
 dumpAndPackPostgres() {
     echo "\n##### Dumping and Packing Digituz Database #####\n"
-    cd /home/brunokrebs/digituz-backups/
-    PGPASSWORD=ZRynpioC4Zzt8MeR \
-        pg_dump -U brunokrebs -d digituz > digituz-postgres.dump
+    
+    cd /tmp/
+    
+    PGPASSWORD=spbGskUwcgv6RuNqJrcn3KqMqj \
+    pg_dump \
+    --host=databases.digituz.com.br \
+    --port=7432 \
+    --username=digituz-db-user \
+    --dbname=digituz \
+    > digituz-postgres.dump
+    
     tar -czvf $DATE-digituz-postgres.dump.tar.gz digituz-postgres.dump
     rm digituz-postgres.dump
 }
 
-moveToSpace() {
-    echo "\n##### Moving the backup file to DigitalOcean Spaces #####\n"
-    cd /home/brunokrebs/digituz-backups/
-    s3cmd put $DATE-digituz-postgres.dump.tar.gz s3://digituz-backups/postgresql/
+dumpAndMongo() {
+    echo "\n##### Dumping and Packing Digituz Database #####\n"
+    
+    cd /tmp/
+    
+    mongodump \
+    --host databases.digituz.com.br \
+    --port 23987 \
+    --authenticationDatabase admin \
+    --username frida-kahlo-website-user \
+    --password yNYyKfW2rQzVrmifzsNjA8aVHX \
+    --db frida \
+    --archive=$DATE-frida-dump.gz \
+    --gzip
 }
 
-dumpAndPackPostgres && moveToSpace
+moveToSpace() {
+    echo "\n##### Moving the backup file to DigitalOcean Spaces #####\n"
+    cd /tmp/
+    s3cmd put $DATE-digituz-postgres.dump.tar.gz s3://digituz-backups/postgresql/
+    s3cmd put $DATE-frida-dump.gz s3://digituz-backups/mongo/
+}
+
+dumpAndPackPostgres && dumpAndMongo && moveToSpace
