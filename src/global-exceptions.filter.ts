fad94dc5179cdcd8d32003e9918344273fef9026
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-04-07 19:04:30 -0300

    gerando alert na api

--- src/global-exceptions.filter.ts
+++ src/global-exceptions.filter.ts
@@ -6,6 +6,7 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { AppLogger } from './logger/app-logger.service';
+import fetch from 'node-fetch';
 
 @Catch()
 export class GlobalExceptionsFilter implements ExceptionFilter {
@@ -14,7 +15,7 @@ export class GlobalExceptionsFilter implements ExceptionFilter {
     this.logger = logger;
   }
 
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
@@ -23,8 +24,7 @@ export class GlobalExceptionsFilter implements ExceptionFilter {
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    // when a request fails, we add details to DataDog
+    // when a request fails, we add details to DataDog and send Alert to Slack
     this.logger.error({
       req: {
         user: request.user,
@@ -43,5 +43,50 @@ export class GlobalExceptionsFilter implements ExceptionFilter {
       timestamp: new Date().toISOString(),
       path: request.url,
     });
+
+    const url =
+      process.env.NODE_ENV === 'production'
+        ? process.env.ERROR_CHANNEL
+        : process.env.TEST_ALERTS_CHANNEL;
+
+    const formatedMessage = {
+      text: 'Falha em Digituz Dashboard API',
+      attachments: [
+        {
+          text: `Houve erro inesperado ao executar uma requisição`,
+          fallback: 'Não a erro',
+          callback_id: 'wopr_game',
+          color: '#3AA3E3',
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'goDatadog',
+              text: 'Ir para os logs do datadog',
+              style: 'danger',
+              type: 'button',
+              value: 'war',
+              url: 'https://app.datadoghq.com/logs?index=%2A&query=',
+            },
+          ],
+        },
+      ],
+    };
+
+    try {
+      fetch(url, {
+        mode: 'no-cors',
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(formatedMessage),
+      })
+        .then(() => console.log('sucesso'))
+        .catch(err => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
