08a1d09036beb0fe3950c78cf0cf837eeaba45fe
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-04-14 16:25:42 -0300

    removing more imports

--- src/bling/bling.module.ts
+++ src/bling/bling.module.ts
@@ -2,24 +2,15 @@ import { Module, HttpModule } from '@nestjs/common';
 import { BlingService } from './bling.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../products/entities/product.entity';
-import { ProductVariation } from '../products/entities/product-variation.entity';
-import { Inventory } from '../inventory/inventory.entity';
 import { InventoryModule } from '../inventory/inventory.module';
 import { ProductComposition } from '../products/entities/product-composition.entity';
 import { SaleOrder } from '../sales-order/entities/sale-order.entity';
-import { SaleOrderItem } from '../sales-order/entities/sale-order-item.entity';
+
 @Module({
   imports: [
     InventoryModule,
     HttpModule,
-    TypeOrmModule.forFeature([
-      Product,
-      ProductVariation,
-      ProductComposition,
-      Inventory,
-      SaleOrder,
-      SaleOrderItem,
-    ]),
+    TypeOrmModule.forFeature([Product, ProductComposition, SaleOrder]),
   ],
   providers: [BlingService],
   exports: [BlingService],
